31154fcb37355686db02d89c4a98705b6e89bc1b
Vovanchu <106510219+Vovanchu@users.noreply.github.com>
2024-02-26 00:35:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // script.js
-
 let slideIndex = 0;
 const slides = document.querySelectorAll('.carousel-img');
 
@@ -24,3 +23,26 @@ function prevSlide() {
 setInterval(() => {
     nextSlide();
 }, 15000);
+
+/*Copy password*/
+document.getElementById("wifiPassword").addEventListener("click", function() {
+    var password = this.innerText;
+    navigator.clipboard.writeText(password).then(function() {
+        console.log("Wi-Fi password copied to clipboard");
+        alert("Wi-Fi password copied to clipboard: " + password);
+    }, function(err) {
+        console.error("Failed to copy Wi-Fi password: ", err);
+        alert("Failed to copy Wi-Fi password. Please try again.");
+    });
+});
+
+
+/*Email*/
+document.getElementById("emailLink").addEventListener("click", function() {
+    var emailAddress = this.innerText;
+    // Створюємо посилання для відправки листа
+    var mailtoLink = "mailto:" + emailAddress;
+    // Відкриваємо посилання в новому вікні
+    window.open(mailtoLink, '_blank');
+});
+
